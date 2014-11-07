e4cad90b450793ae6229b37b2fa9459f7487ee51
toannguyenle <toan@me.com>
2014-11-07 10:31:41 -0800

    no ar

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
 # END FIXING
 
 # Require the gems listed in Gemfile, including any gems
@@ -32,10 +31,5 @@ module Routespractice
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
-    # MORE FIXING
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
-    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
   end
 end
